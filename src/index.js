a773ca99ea5124d456a403d48a5e88471567a036
Dow Drake <dowdrake@gmail.com>
2022-04-27 16:53:03 -0700

    Removed service-worker.js
    work around 'Can't find self.__WB_MANIFEST in your SW source' issue
    which was preventing build from compiling. Need to research this...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
-import * as serviceWorker from './service-worker'
+// import * as serviceWorker from './service-worker'
 
 import './styling/index.scss'
 
@@ -13,4 +13,4 @@ root.render(<App tab="home" />);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// serviceWorker.unregister()
